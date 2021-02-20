d93d95af5d1ac0144160c0eeeef6f10c7a45c17b
spdevi <spneuro01@gmail.com>
2021-02-20 23:45:58 +0900

    Add missing semicolon

---
+++ pages/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+type CartProps = {
+    itemInCartAmount: number;
+}
+
+type CartState = {
+    itemInCartAmount: number;
+}
+
+class AddToCart extends React.Component<CartProps, CartState> {
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import * as React from 'react';
 
 export const HelloComponent = () => {
   return <h2>Hello component !</h2>;
